aa0905e736fb0049a6a6948f3e4451a4979f5dc7
Bryan E. Burr <beburr@stanford.edu>
2015-05-22 14:14:01 -0700

    item pipeline now properly converts item data types into the data types that the sqlite database is expecting (string, int, etc.)

--- wiki_companysize/spiders/wiki_spider.py
+++ wiki_companysize/spiders/wiki_spider.py
@@ -21,14 +21,15 @@ class WikiSpiderSpider(CrawlSpider):
         tables = sel.xpath('//*[@id="mw-content-text"]/table')
         # the data we're looking for is contained within a table (we don't know which table, so we gotta check 'em all):
         for table in tables:
-            # check <tr>'s for "Number of employees" in a <th>:
+            # check <tr>'s for string "Number of employees" in a <th><div>:
             for tr in table.xpath('.//tr'):
                 if tr.xpath('.//th/div/text()').extract() == [u'Number of employees']:
-                    # extract data we want:
+                    # extract data we want - looks like "10,000 (YEAR)":
                     employees_as_of_year = tr.xpath('.//td/text()').extract()[0].split()
-                    i['employees'] = employees_as_of_year[0]
-                    # strip off parentheses from year
-                    i['as_of'] = employees_as_of_year[1].strip('()')
+                    # python is pretty smart, but not smart enough to properly convert strings like "1,000" into integers (darn commas):
+                    i['employees'] = int(employees_as_of_year[0].replace(',', ''))
+                    # strip off parentheses from year and cast to int
+                    i['as_of'] = int(employees_as_of_year[1].strip('()'))
                     i['name'] = sel.xpath('//*[@id="firstHeading"]/text()').extract()[0]
                     # if we've found what we're after, we're done:
                     break
