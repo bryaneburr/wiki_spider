efc478c7a11330a4698d53be1c952d4a330a14a6
Bryan E. Burr <beburr@stanford.edu>
2015-05-22 13:53:19 -0700

    git is behaving strangely

--- wiki_companysize/pipelines.py
+++ wiki_companysize/pipelines.py
@@ -3,8 +3,21 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import sqlite3
+import sqlite3 as sql
 
 class WikiCompanysizePipeline(object):
+	def __init__(self):
+		self.dbcon = sql.connect('results.db')
+		with self.dbcon:
+			cur = self.dbcon.cursor()
+			cur.execute('CREATE TABLE IF NOT EXISTS companies(id INTEGER PRIMARY KEY, name STRING, employees INTEGER, as_of INTEGER);')
+
     def process_item(self, item, spider):
+    	if hasattr(item, 'employees'):
+    		with self.dbcon:
+    			cur = self.dbcon.cursor()
+    			insert = (item['name'], item['employees'], item['as_of'])
+    			# passing a value of NULL to an integer primary key will cause the integer to autoincrement (sqlite3 quirk)
+    			# which is what we want here:
+    			cur.execute('INSERT INTO companies VALUES (NULL, ?, ?, ?)', insert)
         return item
--- wiki_companysize/spiders/wiki_spider.py
+++ wiki_companysize/spiders/wiki_spider.py
@@ -1,16 +1,18 @@
 from scrapy.selector import Selector
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.linkextractors.sgml import LinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from wiki_companysize.items import WikiCompanysizeItem
 
 class WikiSpiderSpider(CrawlSpider):
     name = 'wiki_spider'
     allowed_domains = ['en.wikipedia.org']
-    start_urls = ['http://www.en.wikipedia.org/']
+    start_urls = [#'http://www.en.wikipedia.org/'
+                    'en.wikipedia.org/wiki/Apple_Inc.'
+                 ]
 
     # follow all links on page that point to any valid wikipedia page
     rules = (
-        Rule(SgmlLinkExtractor(allow=r'/*'), callback='parse_item', follow=True, restrict_xpaths=('//*[@id="mw-content-text"]')),
+        Rule(LinkExtractor(allow=r'/*'), callback='parse_item', follow=False, restrict_xpaths=('//*[@id="mw-content-text"]')),
     )
 
     def parse_item(self, response):
@@ -19,7 +21,7 @@ class WikiSpiderSpider(CrawlSpider):
         tables = sel.xpath('//*[@id="mw-content-text"]/table')
         # the data we're looking for is contained within a table (we don't know which table, so we gotta check 'em all):
         for table in tables:
-            # check <tr>'s for "Number of employees" in table header:
+            # check <tr>'s for "Number of employees" in a <th>:
             for tr in table.xpath('.//tr'):
                 if tr.xpath('.//th/div/text()').extract() == [u'Number of employees']:
                     # extract data we want:
